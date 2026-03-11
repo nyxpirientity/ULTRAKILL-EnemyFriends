7ded5f93943d9c1126ba100becb3fbc47ee61353
Nixya <64989593+nyxpirientity@users.noreply.github.com>
2026-03-10 23:49:27 -0700

    Initial Commit

---
+++ Source/EnemyFriendsPlugin.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using BepInEx;
+using System;
+
+namespace Nyxpiri.ULTRAKILL.EnemyFriends
+{
+    public static class Cheats
+    {
+        public const string GiveEnemiesFriends = "nyxpiri.give-enemies-friends";
+    }
+
+    [BepInPlugin("com.nyxpiri.bepinex.plugins.ultrakill.enemy-friends", "Enemy Friends", "0.0.0.1")]
+    [BepInProcess("ULTRAKILL.exe")]
+    public class EnemyFriends : BaseUnityPlugin
+    {
+        protected void Awake()
+        {
+            Log.Initialize(Logger);
+            EnemyFriendIdentifier.Initialize();
+            NyxLib.Cheats.ReadyForCheatRegistration += RegisterCheats;
+        }
+
+        private void RegisterCheats(CheatsManager cheatsManager)
+        {
+            cheatsManager.RegisterCheat(new ToggleCheat(
+                "Give Enemies Friend(s)! c:", 
+                Cheats.GiveEnemiesFriends,
+                onDisable: (cheat) =>
+                {
+                },
+                onEnable: (cheat, manager) =>
+                {
+                }
+            ), "misc");
+        }
+
+        protected void Start()
+        {
+        }
+
+        protected void Update()
+        {
+
+        }
+
+        protected void LateUpdate()
+        {
+
+        }
+    }
+}
